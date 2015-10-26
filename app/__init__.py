52c952fb64d2065086c5938c819e06566bda18ae
77720616 <77720616@qq.com>
2015-10-27 00:15:31 +0800

    mongoengine import

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 __author__ = 'nan'
 from flask import Flask
 from flask.ext.mongoengine import MongoEngine
+from mongoengine import connect
 
 app = Flask(__name__)
 app.config.from_object('config')
 
-db = MongoEngine(app)
+# db = MongoEngine(app)
+db = connect('todo')
 
 from app import views,models
